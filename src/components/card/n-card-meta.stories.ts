139194686ef59a2d049a765a35010cd95adf55f3
James Gao <392041299@qq.com>
2022-06-14 17:41:40 +0800

    chore: (wip) `Card`

--- src/components/card/n-card-meta.stories.ts
+++ src/components/card/n-card-meta.stories.ts
@@ -1,11 +1,13 @@
 import type { Meta, Story } from '@storybook/vue3'
 
+import NAvatar from '../avatar/n-avatar.vue'
 import NCard from './n-card.vue'
 import NCardMeta from './n-card-meta.vue'
 
 const meta: Meta = {
   title: '设计系统/数据展示/卡片元信息 CardMeta',
   component: NCardMeta,
+  decorators: [() => ({ template: '<div class="w-[300px]"><story /></div>' })],
 }
 
 export default meta
@@ -14,7 +16,19 @@ export const Default: Story = (args) => ({
   components: {
     NCard,
     NCardMeta,
+    NAvatar,
   },
   setup: () => ({ args }),
-  template: ``,
+  template: `
+    <n-card>
+      <n-card-meta v-bind="args">
+        <template #avatar>
+          <n-avatar src="https://joeschmoe.io/api/v1/jess" />
+        </template>
+      </n-card-meta>
+    </n-card>
+  `,
 })
+Default.args = {
+  title: '元信息标题',
+}
--- src/components/card/n-card-meta.vue
+++ src/components/card/n-card-meta.vue
@@ -1,3 +1,19 @@
 <template>
-  <div></div>
+  <div class="flex my-[-4px]">
+    <div v-if="$slots.avatar" class="mr-md">
+      <slot name="avatar" />
+    </div>
+    <div v-if="title || $slots.title" class="overflow-hidden">
+      <div v-if="title || $slots.title">
+        <slot name="title">{{ title }}</slot>
+      </div>
+    </div>
+  </div>
 </template>
+
+<script lang="ts" setup>
+defineProps({
+  /** 标题 */
+  title: { type: String, default: '' },
+})
+</script>
